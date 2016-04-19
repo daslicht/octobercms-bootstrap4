b201e06a5399faeacd79760efd9fc9842964eed9
daslicht <daslicht@ansolas.de>
2016-04-19 22:22:03 +0200

    fixed render call

---
+++ storage/cms/twig/0a/0ad6aab2f3bdc8101a6dd00fc3985dcd08901e19ba70dfc7fbb18658cfa0e844.php
@@ -0,0 +1,32 @@
+<?php
+
+/* /Applications/AMPPS/www/october/plugins/daslicht/contact/components/contact/result.htm */
+class __TwigTemplate_6233eb1527f539f85081ce003663d546a762bbba4331fbd9b076cfbaa11422e0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "result comes here ";
+    }
+
+    public function getTemplateName()
+    {
+        return "/Applications/AMPPS/www/october/plugins/daslicht/contact/components/contact/result.htm";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* result comes here */
